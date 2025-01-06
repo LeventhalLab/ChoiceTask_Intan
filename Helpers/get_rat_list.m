c17d53a43684f4ebd608b63e6fa33852f4690ca2
Dan Leventhal <dleventh@med.umich.edu>
2025-01-06 11:32:14 -0500

    updates to scalogram analysis

--- Helpers/get_rat_list.m
+++ Helpers/get_rat_list.m
@@ -5,22 +5,39 @@ function [ratIDs, ratIDs_goodhisto] = get_rat_list(rats_to_analyze, parent_direc
 
 % rat_nums = [326, 327, 372, 374, 376, 378, 379, 394, 395, 396, 411, 412, 413, 419, 420, 425];
 
+
+
+ratnums_with_bad_histo = [374, 376, 396, 413];
+n_rats_with_good_histo = 0;
 if ischar(rats_to_analyze)
     if strcmpi(rats_to_analyze, 'all')
         % add code here to use all the folders if needed, but I think this
         % is an obsolete function
+        rat_folders = get_rat_folders(parent_directory);
+        n_rats = length(rat_folders);
+        ratIDs = cell(n_rats, 1);
+        for i_rat = 1 : length(rat_folders)
+            [~, ratID, ~] = fileparts(rat_folders{i_rat});
+            rat_num = str2num(ratID(2:end));
+            ratIDs{i_rat} = ratID;
+            if ~ismember(rat_num, ratnums_with_bad_histo)
+                n_rats_with_good_histo = n_rats_with_good_histo + 1;
+                ratIDs_goodhisto{n_rats_with_good_histo} = ratID;
+            end
+            % rat_nums(i_rat) = str2num(ratID(2:end));
+        end
     end
 end
 
-ratnums_with_bad_histo = [374, 376, 396, 413];
-
-rats_with_good_histo = 0;
+n_rats_with_good_histo = 0;
 for i_rat = 1 : length(rats_to_analyze)
     ratIDs{i_rat} = sprintf('R%04d', rats_to_analyze(i_rat));
 
     if ~ismember(rats_to_analyze(i_rat), ratnums_with_bad_histo)
-        rats_with_good_histo = rats_with_good_histo + 1;
+        n_rats_with_good_histo = n_rats_with_good_histo + 1;
 
-        ratIDs_goodhisto{rats_with_good_histo} = ratIDs{i_rat};
+        ratIDs_goodhisto{n_rats_with_good_histo} = ratIDs{i_rat};
     end
+    % rat_nums(i_rat) = str2num(ratIDs{i_rat}(2:end));
 end
+
--- intan_choice_task_navigation_utils/get_rat_folders.m
+++ intan_choice_task_navigation_utils/get_rat_folders.m
@@ -1,6 +1,25 @@
 function rat_folders = get_rat_folders(parent_directory)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+test_string = fullfile(parent_directory, 'R*');
+folder_list = dir(test_string);
+
+n_poss_folders = length(folder_list);
+n_rat_folders = 0;
+ratID_pattern = 'R' + digitsPattern(4);
+
+for i_folder = 1 : n_poss_folders
+
+    if folder_list(i_folder).isdir
+        if matches(folder_list(i_folder).name, ratID_pattern)
+            n_rat_folders = n_rat_folders + 1;
+            rat_folders{n_rat_folders} = folder_list(i_folder).name;
+        end
+    end
+
+end
+
+if ~exist('rat_folders', 'var')
+    rat_folders = {};
 end
