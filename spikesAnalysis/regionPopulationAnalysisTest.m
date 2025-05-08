c2223e2a1be5ecb1778eae0954863f0d8de1d109
Ryan <111521362+ryanlash@users.noreply.github.com>
2025-05-08 15:56:15 -0400

    unit classification

--- spikesAnalysis/regionPopulationAnalysisTest.m
+++ spikesAnalysis/regionPopulationAnalysisTest.m
@@ -3,7 +3,7 @@ allEntries=dir(parentDir);
 % Filter only directories, excluding '.' and '..'
 isSubFolder = [allEntries.isdir] & ~ismember({allEntries.name}, {'.', '..'});
 regionsAvailable = {allEntries(isSubFolder).name};
-
+behavioralFeature='correct_cuedleft'
 
 
 for i=1:length(regionsAvailable)
@@ -18,21 +18,44 @@ for i=1:length(regionsAvailable)
         regionUnits.regionMetrics=struct();
     end
     regionUnits.regionMetrics.numUnits=length(unitNames);
+    controlEphysProperties=table();
+    lesionEphysProperties=table();
+    controlUnitFRs=[];
+    lesionUnitFRs=[];
+    %eventsOverlayed(regionUnits,unitNames,behavioralFeatures)
     for u=1:length(unitNames)
         unitID=unitNames{u};
         unitFR=regionUnits.(unitID).unitMetrics.meanFiringRate;
         spikeTimes=regionUnits.(unitID).unitMetrics.clusterSpikes;
+        ephysProp=regionUnits.(unitID).unitMetrics.ephysProperties;
+        acg=regionUnits.(unitID).unitMetrics.unitACG;
+        ISIs=diff(spikeTimes);
         if strcmp(regionUnits.(unitID).unitMetrics.treatement,'control')
             controlUnitFRs(u)=unitFR;
         else
             lesionUnitFRs(u)=unitFR;
         end
-        unitClass=unitClassificationByFR(unitFR,spikeTimes);
+        if strcmp(regionUnits.(unitID).unitMetrics.treatement, 'control')
+            controlEphysProperties = [controlEphysProperties; ephysProp];
+        else
+            lesionEphysProperties = [lesionEphysProperties; ephysProp];
+        end
+        %unitClass=unitClassificationByFR(unitFR,spikeTimes);
             % if regionUnits.(unitID).unitMetrics.
         % controlRegionFRs(u)=unitFR;
     end
-    regionUnits.regionMetrics.controlUnitFRs=controlUnitFRs;
-    regionUnits.regionMetrics.lesionUnitFRs=lesionUnitFRs;
+    if ~isempty(controlUnitFRs)
+        regionUnits.regionMetrics.controlUnitFRs=controlUnitFRs;
+    end
+    if ~isempty(lesionUnitFRs)
+        regionUnits.regionMetrics.lesionUnitFRs=lesionUnitFRs;
+    end
+    if ~isempty(controlEphysProperties)
+        regionUnits.regionMetrics.controlEphysProperties=controlEphysProperties;
+    end
+    if ~isempty(lesionEphysProperties)
+        regionUnits.regionMetrics.lesionEphysProperties=lesionEphysProperties;
+    end
     mean3RegionFR=movmean(regionFRs,3);
     %meanRegionFR=
     stdMeanRegionFR=std(mean3regionFRs);
